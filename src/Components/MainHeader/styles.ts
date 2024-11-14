1e4047426e6661f25ee38e2e755d63aa81de0981
Antonio Luiz Carvalho Turano Filho <turano@usp.br>
2024-11-14 10:48:00 -0300

    fix(main-header): resolve overlapping issue with buttons
    - changed its color

--- src/Components/MainHeader/styles.ts
+++ src/Components/MainHeader/styles.ts
@@ -7,16 +7,14 @@ export const Logo = styled.img`
 
 export const HeaderContainer = styled.header`
   display: flex;
+  flex-direction: row;
   align-items: center;
   justify-content: space-between;
   padding: 10px 20px;
-  background-color: #fff;
+  background-color: #ffc100;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  position: fixed;
   width: 100%;
   top: 0;
   left: 0;
-  z-index: 1000;
   box-sizing: border-box;
-  background-color: #ffc100;
 `;
